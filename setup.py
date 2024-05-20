3bae641b97c79132858a50a47253aa219e612d49
Berk Yalcinkaya <berkyalcinkaya55@gmail.com>
2024-05-20 12:10:44 -0700

    FIX: requires version and README install instructions

--- setup.py
+++ setup.py
@@ -7,33 +7,35 @@ requires = [
         'networkx~=2.6.2',
         'node2vec~=0.4.4',
         'scipy~=1.7.3',
-        'nni~=2.4'
+        'nni~=2.4',
+        "PyQt5",
+        "matplotlib==3.8"
     ]
 
 extras_require={
         'cpu': [
-            'torch==1.8.0',
+            'torch==1.8.1',
             'torchvision==0.9.1',
             'torchaudio==0.8.1'
         ],
         'gpu': [
-            'torch==1.8.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html',
-            'torchvision==0.9.1+cu101 -f https://download.pytorch.org/whl/torch_stable.html',
-            'torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html'
+            'torch==1.8.1+cu101',
+            'torchvision==0.9.1+cu101',
+            'torchaudio==0.8.1'
         ]
     }
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-packages = ["BrainGraphStudio", "BrainGraphStudio.gui", "BrainGraphStudio.gui.ims",
+packages = ["BrainGraphStudio", "BrainGraphStudio.gui", "BrainGraphStudio.gui.ims", "BrainGraphStudio.train",
             "BrainGraphStudio.models", "BrainGraphStudio.models.brainGNN", "BrainGraphStudio.BrainGB",
-            "BrainGraphStudio.src", "BrainGraphStudio.src.dataset", "BrainGraphStudio.src.models", 
-            "BrainGraphStudio.src.utils"]
+            "BrainGraphStudio.BrainGB.src", "BrainGraphStudio.BrainGB.src.dataset", "BrainGraphStudio.BrainGB.src.models", 
+            "BrainGraphStudio.BrainGB.src.utils"]
 
 setup(
     name = "BrainGraphStudio",
-    version = "0.1.0",
+    version = "0.1.3",
     description = "A GUI-based toolkit for building, training, and optimizing graph neural networks for brain graph analysis",
     author = "Berk Yalcinkaya",
     url = "https://github.com/berkyalcinkaya/BrainGraphStudio",
@@ -44,11 +46,11 @@ setup(
     packages = packages,
     install_requires = requires,
     extras_require=extras_require,
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
-    ),
+    ],
     entry_points = {
         'console_scripts': [
           'bgs = BrainGraphStudio.__main__:main']
